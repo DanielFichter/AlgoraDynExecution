05934c1e0011ad43ec5f9be4c50d20ec36938a8c
Daniel Fichter <fichterdaniel5@gmail.com>
2025-04-07 11:40:25 +0200

    Query path with length 2

--- main.cpp
+++ main.cpp
@@ -3,35 +3,97 @@
 #include "graph.incidencelist/incidencelistvertex.h"
 #include "io/konectnetworkreader.h"
 #include "graph.dyn/dynamicdigraph.h"
+#include "algorithm.reachability.ss/staticbfsssreachalgorithm.h"
 
 #include <iostream>
 #include <fstream>
+#include <algorithm>
+#include <string>
+
+using namespace Algora;
+
+namespace
+{
+    std::string getLastOperationType(const DynamicDiGraph &dynamicGraph)
+    {
+        if (dynamicGraph.lastOpWasVertexAddition())
+        {
+            return "Vertex Addition";
+        }
+        else if (dynamicGraph.lastOpWasVertexRemoval())
+        {
+            return "Vertex Removal";
+        }
+        else if (dynamicGraph.lastOpWasArcAddition())
+        {
+            return "Arc Addition";
+        }
+        else if (dynamicGraph.lastOpWasArcRemoval())
+        {
+            return "Arc Removal";
+        }
+        else
+        {
+            return "No Operation";
+        }
+    }
+
+    DynamicDiGraph readKroneckerGraph(const std::string &filename)
+    {
+        DynamicDiGraph dynamicGraph;
+        KonectNetworkReader graphReader;
+        std::ifstream graphInput{filename};
+        graphReader.setInputStream(&graphInput);
+        graphReader.provideDynamicDiGraph(&dynamicGraph);
+        return dynamicGraph;
+    }
+}
 
 int main()
 {
-    using namespace Algora;
+    SimpleESTree<false> simpleES;
 
-    SimpleESTree<false> algorithm;
-
-    KonectNetworkReader graphReader;
-    std::ifstream graphInput{"graphs/answers"};
-    graphReader.setInputStream(&graphInput);
-    DynamicDiGraph dynamicGraph;
-    graphReader.provideDynamicDiGraph(&dynamicGraph);
+    DynamicDiGraph dynamicGraph = readKroneckerGraph("graphs/gnutella-25");
     const auto graph = dynamicGraph.getDiGraph();
-    algorithm.setAutoUpdate(true);
-    algorithm.setGraph(graph);
+    simpleES.setGraph(graph);
+    size_t operationIndex = 0;
     while (graph->getSize() < 1)
     {
         dynamicGraph.applyNextOperation();
+        operationIndex++;
     }
     const auto source = graph->vertexAt(0);
-    algorithm.setSource(source);
+    simpleES.setSource(source);
 
     std::cout << std::boolalpha;
-    std::cout << "Algorithm is perpared: " << algorithm.prepare() << std::endl;
+    std::cout << "Algorithm is perpared: " << simpleES.prepare() << std::endl;
 
-    algorithm.unsetGraph();
+    constexpr size_t finalOperationIndex = 223057LLU;
+    const std::string targetVertexName = "117";
+    const Vertex *vertex = nullptr;
+    for (size_t operationCount = 0; operationCount < finalOperationIndex + 1; operationCount++)
+    {
+        dynamicGraph.applyNextOperation();
+
+        if (dynamicGraph.lastOpWasArcAddition())
+        {
+            for (size_t vertexIndexOffset = 1; vertexIndexOffset < 3; vertexIndexOffset++)
+            {
+                const Vertex *const currentVertex = graph->vertexAt(graph->getSize() - vertexIndexOffset);
+                if (currentVertex->getName() == targetVertexName)
+                {
+                    vertex = currentVertex;
+                }
+            }
+        }
+    }
+
+    const auto path = simpleES.queryPath(vertex);
+
+    std::cout << "after operation " << finalOperationIndex << "(" << getLastOperationType(dynamicGraph) << "): ";
+    std::cout << "is vertex " << targetVertexName << "(id=" << vertex->getName() << ") reachable? " << simpleES.query(vertex) << ", path length: " << path.size() << std::endl;
+
+    simpleES.unsetGraph();
 
     return 0;
 }
