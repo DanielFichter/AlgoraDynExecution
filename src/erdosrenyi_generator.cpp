804d636709a0c775e0a341fd83e173d7be5d2c97
Daniel Fichter <fichterdaniel5@gmail.com>
2025-11-17 09:27:30 +0100

    Add algorithm to generate erdos-renyi graphs

---
+++ src/erdosrenyi_generator.cpp
@@ -0,0 +1,25 @@
+#include "erdosrenyi_generator.hpp"
+#include <cstddef>
+#include <graph.dyn/dynamicdigraph.h>
+#include <random>
+
+DynamicDiGraph generateErdosRenyi(size_t n, size_t m) {
+
+    std::mt19937 randomEngine;
+
+    DynamicDiGraph result;
+
+    for (size_t vertexIndex = 0; vertexIndex < n; vertexIndex++) {
+        result.addVertex(0);
+    }
+
+    for (size_t arcIndex = 0; arcIndex < m; arcIndex++) {
+        size_t vertexIndex1 = randomEngine() % n;
+        size_t vertexIndex2 = randomEngine() % n;
+
+        result.addArc(vertexIndex1, vertexIndex2, 0);
+    }
+    
+
+    return result;
+}
---
+++ src/erdosrenyi_generator.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <graph.dyn/dynamicdigraph.h>
+
+using namespace Algora;
+
+DynamicDiGraph generateErdosRenyi(size_t n, size_t m);
